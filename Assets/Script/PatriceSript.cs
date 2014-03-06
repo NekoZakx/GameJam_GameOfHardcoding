d786627ad97c80805497a2fdd4bf4b98100f701f
Headkiller <patrice_pedneault@live.ca>
2014-03-06 14:15:58 -0500

    Patrice Script Test

---
+++ Assets/Script/PatriceSript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatriceSript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
